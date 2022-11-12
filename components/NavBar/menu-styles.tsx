63c72448f0821623cebd150344e4773591a78200
bengu ozakinci <mochi@bengus-MacBook-Air.local>
2022-11-12 20:07:29 +0300

    Refactor nav wrapper

--- components/NavBar/menu-styles.tsx
+++ components/NavBar/menu-styles.tsx
@@ -3,14 +3,10 @@ import styled from "styled-components";
 
 export const DesktopMenuWrapper = styled.div`
   display: none;
-  padding-top: 1rem;
 
   @media only screen and (min-width: ${breakpoints.sm}px) {
     display: flex;
     justify-content: space-evenly; 
-    padding-bottom: 1rem;
-    text-align: center;
-    line-height: 2rem;
   }
 `;
 
@@ -18,15 +14,22 @@ export const NavWrapper = styled.nav`
   background: brown;
   color: white;
   font-size: 2rem;
+
+  @media only screen and (min-width: ${breakpoints.sm}px) {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    text-align: center;
+    line-height: 2rem;
+  }
 `;
 
 export const MobileMenuWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 0.75rem;
   padding-bottom: 1.25rem;
   padding-top: 1.25rem;
   padding-left: 1.25rem;
-  gap: 0.75rem;
 
   a {
     margin-right: 1.25rem;
