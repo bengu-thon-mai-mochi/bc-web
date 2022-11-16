2c2246e386b7f9589d45a4486d64056ef38caac4
bengu ozakinci <mochi@bengus-MacBook-Air.local>
2022-11-16 15:31:14 +0300

    Style thumbnails according to the theme

--- components/Blog/thumbnail.tsx
+++ components/Blog/thumbnail.tsx
@@ -6,21 +6,23 @@ const ImgWrapper = styled.div`
   position: relative;
   width: 175px;
   height: 175px;
-  border-radius: 5px;
-  border: 1px solid gainsboro;
-  background-color: brown;
+  border-radius: 25px;
+  font-family: "Outfit", sans-serif;
+  border: 1px dashed #73AD21;
   color: silver;
   display: flex;
   justify-content: center;
   align-items: center;
 
   img {
-    border-radius: 5px;
+    border-radius: 25px;
   }
 
   @media only screen and (min-width: ${breakpoints.sm}px) {
     min-width: 200px;
     height: 200px;
+    border-radius: 25px;
+    padding: 15px;
   }
 `;
 
